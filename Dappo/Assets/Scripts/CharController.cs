870a89a728eb1cc5bb4c10fc600bef79683c03dc
MichaelFell <mfeller18@gmail.com>
2018-04-02 15:36:05 -0600

    Initial commit from git bash

---
+++ Dappo/Assets/Scripts/CharController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharController : MonoBehaviour {
+
+    public float inputDelay = 0.1f;
+    public float fowardVel = 12;
+    public float rotateVel = 100;
+
+    Quaternion targetRotation;
+    Rigidbody rBody;
+
+    float forwardInput;
+    float turnInput;
+
+    public Quaternion TargetRotation
+    {
+        get { return targetRotation; }
+    }
+
+    void Start()
+    {
+        targetRotation = transform.rotation;
+        rBody = GetComponent<Rigidbody>();
+    }
+
+    void GetInput()
+    {
+
+    }
+
+    void Update()
+    {
+        GetInput();
+        Turn();
+    }
+
+    void FixedUpdate()
+    {
+        Run();
+    }
+
+    void Run()
+    {
+
+    }
+
+    void Turn()
+    {
+
+    }
+}
